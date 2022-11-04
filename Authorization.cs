e4a2b61855813057207d7f28b7bf56a6fcc1c6b2
derevolaz1k <0_the_kick_0@mail.ru>
2022-11-04 21:11:44 +0500

    Добавьте файлы проекта.

---
+++ Authorization.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TryCatchLab001
+{
+    class Authorization
+    {
+        private List<string> Names = new List<string>();
+        
+        public int readNames(string _path)
+        {
+            try
+            {
+                StreamReader myStreamReader = new StreamReader(_path);
+                string _buffer = "";
+                do
+                {
+                    _buffer = myStreamReader.ReadLine();
+                    if (_buffer == null)
+                    {
+                        return 0;
+                    }
+                    Names.Add(_buffer);
+                    Console.WriteLine(_buffer);
+                } while (_buffer != null);
+            }
+            catch (Exception ex001)
+            {
+                
+                Console.WriteLine(ex001.Message);
+            }
+            return Names.Count;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TryCatchLab001
+{
+    class Program
+    {
+        static public void NameCheck()
+        {
+            Regex _regex = new Regex(@"\s\d{1}\w*");
+            string _text = Console.ReadLine();
+            MatchCollection _matches = _regex.Matches(_text);
+            Console.WriteLine( _matches);
+
+        }
+        static void Main(string[] args)
+        {
+            NameCheck();
+            try
+            {
+                //Lord Voldemort
+                if (args[0] == "Voldemort")
+                {
+                    throw new Exception("Тсс! Это имя нельзя произносить!");
+                }
+                else
+                {
+                    if (args[0] == "Lord_Voldemort")
+                    {
+                        throw new Exception("Тсс! Это имя ТОЖЕ нельзя произносить!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Здравствуйте, {0}!", args[0]);
+                    }
+
+                }
+
+            }
+            catch (Exception ex001) when (ex001.Message == "Тсс! Это имя нельзя произносить!")
+            {
+                //Console.WriteLine("Случилось {0}", ex001.ToString());
+                Console.WriteLine("Непроизносимое имя: {0}", ex001.Message);
+                //throw;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Нет аргумента командной строки");
+            }
+            catch (Exception ex002)
+            {
+                Console.WriteLine("Возникло: {0}", ex002.ToString());
+            }
+
+            //finally
+            //{
+
+            //}
+
+            Authorization myAuthorization = new Authorization();
+            string _path = "Dwarfs.txt";
+            int _count = myAuthorization.readNames(_path);
+            Console.WriteLine("В файле {0} {1} строк", _path, _count);
+
+        }
+    }
+}
